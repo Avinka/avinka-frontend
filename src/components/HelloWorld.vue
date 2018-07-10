32988978e46ad0bc766d680e456e9233ad55b9b7
andi <andi@zerotired.com>
2018-07-10 17:45:35 +0200

    copy in one working example using echarts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li><router-link to="/demo06">Demo 06</router-link></li>
+    </ul>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Demo06 from '@/components/Demo06'
 
 Vue.use(Router)
 
