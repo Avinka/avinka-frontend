a862c786954fcf5d753e00ca3e83229ec25ff7ba
andi <andi@zerotired.com>
2018-07-13 16:26:06 +0200

    use the other modal lib

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VModal from 'vue-js-modal';
 
+Vue.use(VModal, { dynamic: true });
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-import ModalOverlayComp from '@/components/ModalOverlayComp';
 import Demo06 from '@/components/Demo06';
 
 Vue.use(Router);
@@ -16,10 +15,6 @@ export default new Router({
       path: '/demo06',
       name: 'Demo06',
       component: Demo06
-    }, {
-      path: '/overlay',
-      name: 'ModalOverlayComp',
-      component: ModalOverlayComp
     }
   ]
 });
