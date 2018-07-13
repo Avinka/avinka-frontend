541397616688fbdd734bbbf64090443324fd49ba
andi <andi@zerotired.com>
2018-07-13 16:26:06 +0200

    use the other modal lib

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,57 @@
 <template>
   <div class="hello">
+
     <h1>{{ msg }}</h1>
     <ul>
-      <li><router-link to="/overlay">Overlay</router-link></li>
+      <li><button id="show-modal" @click="showModal()">Show Modal</button></li>
       <li><router-link to="/demo06">Demo 06</router-link></li>
     </ul>
+
+    <modals-container/>
   </div>
 </template>
 
 <script>
+import VModal from 'vue-js-modal';
+import ModalOverlayComp from '@/components/ModalOverlayComp';
+
+const _data = {
+  msg: 'Welcome to Your Vue.js App'
+};
+
 export default {
   name: 'HelloWorld',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    };
+    return _data;
+  },
+  // add here, what Vue should load for us
+  components: {
+    // TODO andi: this does not work, we need to put it in `main.js`
+    VModal
+  },
+  methods: {
+    showModal() {
+      // sweet, load our component in the managed overlay here
+      this.$modal.show(ModalOverlayComp);
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  $modal_width: 450px;
+  $modal_height: 350px;
+  $modal_z_index: 10000;
+
+  .v--modal-box {
+    border-radius: 2px;
+    box-shadow: 0 2px 18px rgba(188, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+</style>
+
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -36,3 +68,5 @@ a {
   color: #42b983;
 }
 </style>
+
+
--- src/components/ModalOverlayComp.vue
+++ src/components/ModalOverlayComp.vue
@@ -1,18 +1,14 @@
 <template>
-  <!--
-   template for the modal component
-   see: https://vuejs.org/v2/examples/modal.html
-   -->
-  <transition name="modal" appear v-if="overlayIsOn">
-    <div class="modal-container">
-      Hello Overlay
-    </div>
-  </transition>
+  <div class="modal-container">
+    <h2>Hello Overlay</h2>
+    <button class="modal-default-button" @click="$emit('close')">
+      OK
+    </button>
+  </div>
 </template>
 
 <script>
   const _data = {
-    'overlayIsOn': false,
     base: process.env.ASSETS_PUBLIC_PATH
   };
 
@@ -32,73 +28,15 @@
      */
     mounted () {
       console.log('ModalOverlayComp mounted');
-
-      const _this = this;
-      _this.$nextTick(() => {
-        // Code that will run only after the
-        // entire view has been rendered
-
-        setTimeout(() => {
-          console.log('ModalOverlayComp turned ON');
-          _this.overlayIsOn = true;
-        }, 200);
-      });
     }
-
   };
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  $modal_width: 450px;
-  $modal_height: 350px;
-  $modal_z_index: 10000;
-
   .modal-container {
-    width: $modal_width;
-    margin: 0px auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-    transition: all .3s ease;
-    font-family: Helvetica, Arial, sans-serif;
-  }
-
-  .modal-header h3 {
-    margin-top: 0;
-    color: #42b983;
-  }
-
-  .modal-body {
-    margin: 20px 0;
-  }
-
-  .modal-default-button {
-    float: right;
-  }
-
-  /*
-   * The following styles are auto-applied to elements with
-   * transition="modal" when their visibility is toggled
-   * by Vue.js.
-   *
-   * You can easily play with the modal transition by editing
-   * these styles.
-   */
-
-  .modal-enter {
-    opacity: 0;
-  }
-
-  .modal-leave-active {
-    opacity: 0;
-  }
-
-  .modal-enter .modal-container,
-  .modal-leave-active .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
+    padding-left: 10px;
+    padding-right: 10px;
   }
 </style>
