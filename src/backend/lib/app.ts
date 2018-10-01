729f629b341def9c43c3b519bde64af40b6dc80e
Fabrizio Schmidt <fabrizio.schmidt@kontist.com>
2018-10-01 16:06:37 +0200

    * initial backend router setup

---
+++ src/backend/lib/app.ts
@@ -0,0 +1,26 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./router";
+
+class App {
+
+  public app: express.Application;
+  public router: Routes = new Routes();
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.router.routes(this.app);
+
+  }
+
+  private config(): void{
+    // support application/json type post data
+    this.app.use(bodyParser.json());
+    //support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+}
+
+export default new App().app;
