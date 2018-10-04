c67d76beb780710be9f7f5912eeeea1adf03a0a8
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-04 15:22:14 +0200

    * crud on dashboard refactored to vuex and added additional methods

--- client/src/api/dashboard.js
+++ client/src/api/dashboard.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 import Vue from 'vue';
 
+// TODO error handling
 export default {
   async getAllDashboards() {
-    // TODO error handling
-    return axios.get('http://localhost:8080/api/dashboard').data;
+    const result = await axios.get('http://localhost:8080/api/dashboard');
+    Vue.$log.debug(result);
+    return result.data;
+  },
+  async deleteDashboard(dashboard) {
+    const result = await axios.delete('http://localhost:8080/api/dashboard/' + dashboard._id);
+    Vue.$log.debug(result);
+  },
+  async createDashboard(dashboard) {
+    const result = await axios.post('http://localhost:8080/api/dashboard/', dashboard);
+    Vue.$log.debug(result);
+    return result.data;
   }
 };
