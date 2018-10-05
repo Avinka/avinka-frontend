5525c397d466fb13437c0c21e70f2199f56c5b9a
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-05 11:09:33 +0200

    * api wrapper

--- client/src/api/dataseries.js
+++ client/src/api/dataseries.js
@@ -3,17 +3,17 @@ import Vue from 'vue';
 
 // TODO error handling
 export default {
-  async getAllGraphs() {
-    const result = await axios.get('http://localhost:8080/api/graph');
+  async getAllDataserieses() {
+    const result = await axios.get('http://localhost:8080/api/dataseries');
     Vue.$log.debug(result);
     return result.data;
   },
-  async deleteGraph(dashboard) {
-    const result = await axios.delete('http://localhost:8080/api/graph/' + dashboard._id);
+  async deleteDataseries(dataseries) {
+    const result = await axios.delete('http://localhost:8080/api/dataseries/' + dataseries._id);
     Vue.$log.debug(result);
   },
-  async createGraph(dashboard) {
-    const result = await axios.post('http://localhost:8080/api/graph/', dashboard);
+  async createDataseries(dataseries) {
+    const result = await axios.post('http://localhost:8080/api/dataseries/', dataseries);
     Vue.$log.debug(result);
     return result.data;
   }
--- client/src/api/graph.js
+++ client/src/api/graph.js
@@ -3,17 +3,17 @@ import Vue from 'vue';
 
 // TODO error handling
 export default {
-  async getAllDashboards() {
-    const result = await axios.get('http://localhost:8080/api/dashboard');
+  async getAllGraphs() {
+    const result = await axios.get('http://localhost:8080/api/graph');
     Vue.$log.debug(result);
     return result.data;
   },
-  async deleteDashboard(dashboard) {
-    const result = await axios.delete('http://localhost:8080/api/dashboard/' + dashboard._id);
+  async deleteGraph(dashboard) {
+    const result = await axios.delete('http://localhost:8080/api/graph/' + dashboard._id);
     Vue.$log.debug(result);
   },
-  async createDashboard(dashboard) {
-    const result = await axios.post('http://localhost:8080/api/dashboard/', dashboard);
+  async createGraph(dashboard) {
+    const result = await axios.post('http://localhost:8080/api/graph/', dashboard);
     Vue.$log.debug(result);
     return result.data;
   }
