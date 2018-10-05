940ec9ff755c260ef8199a12d8c1d63f2b5a9950
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-05 17:22:33 +0200

    Get single dashboard

--- client/src/api/dashboard.js
+++ client/src/api/dashboard.js
@@ -7,6 +7,11 @@ export default {
     Vue.$log.debug(result);
     return result.data;
   },
+  async getDashboard(id) {
+    const result = await axios.get('http://localhost:8080/api/dashboard/' + id);
+    Vue.$log.debug(result);
+    return result.data;
+  },
   async deleteDashboard(dashboard) {
     const result = await axios.delete('http://localhost:8080/api/dashboard/' + dashboard._id);
     Vue.$log.debug(result);
