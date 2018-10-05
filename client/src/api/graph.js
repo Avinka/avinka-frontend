153450cbe8f5994e6a300ad886b0e0a84619abef
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-05 11:09:14 +0200

    * stores and api wrappers for dataseries and graphs

---
+++ client/src/api/graph.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import Vue from 'vue';
+
+// TODO error handling
+export default {
+  async getAllDashboards() {
+    const result = await axios.get('http://localhost:8080/api/dashboard');
+    Vue.$log.debug(result);
+    return result.data;
+  },
+  async deleteDashboard(dashboard) {
+    const result = await axios.delete('http://localhost:8080/api/dashboard/' + dashboard._id);
+    Vue.$log.debug(result);
+  },
+  async createDashboard(dashboard) {
+    const result = await axios.post('http://localhost:8080/api/dashboard/', dashboard);
+    Vue.$log.debug(result);
+    return result.data;
+  }
+};
---
+++ client/src/store/modules/graphs.js
@@ -0,0 +1,46 @@
+import dashboardService from '../../api/dashboard';
+import Vue from 'vue';
+// initial state
+const state = {
+  all: []
+};
+
+// getters
+const getters = {};
+
+// actions
+const actions = {
+  async getAllDashboards ({ commit }) {
+    const dashboards = await dashboardService.getAllDashboards();
+    commit('setDashboards', dashboards);
+  },
+  async deleteDashboard ({ state, commit }, dashboard) {
+    await dashboardService.deleteDashboard(dashboard);
+    commit('deleteDashboard', dashboard);
+  },
+  async createDashboard ({ commit }, dashboard) {
+    const newDashboard = await dashboardService.createDashboard(dashboard);
+    commit('createDashboard', newDashboard);
+  }
+};
+
+// mutations
+const mutations = {
+  setDashboards (state, dashboards) {
+    state.all = dashboards;
+  },
+  deleteDashboard (state, dashboard) {
+    state.all = state.all.filter(item => item._id !== dashboard._id);
+  },
+  createDashboard (state, dashboard) {
+    state.all.push(dashboard);
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
