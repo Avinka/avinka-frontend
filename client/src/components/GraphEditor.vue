0500f1a59bd9fb690fc91cd72928791f857b63f3
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-08 17:19:45 +0200

    * graph editor

--- client/src/components/GraphEditor.vue
+++ client/src/components/GraphEditor.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-      Graph Editor here - EZ - edit {{graph.name}}
       <div>
-        <md-list class="md-double-line" style="display: inline;">
-          <md-subheader>Selectors:</md-subheader>
-          <md-list-item v-for="selector in selectors">
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">{{selector.key}}{{selector.operator}}{{selector.value}}</span>
+        <md-list>
+          <md-list-item>
+            Selectors:
+          </md-list-item>
+          <md-list-item>
+            <div class="md-layout md-gutter md-alignment-left">
+              <div v-for="selector in selectors" class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+                <md-icon>delete</md-icon>{{selector.key}}{{selector.operator}}{{selector.value}}
+              </div>
+            </div>
           </md-list-item>
         </md-list>
       </div>
@@ -29,6 +33,16 @@
             key: 'actor.age',
             value: '45',
             operator: '>='
+          },
+          {
+            key: 'object.age',
+            value: '45',
+            operator: '>='
+          },
+          {
+            key: 'object.type',
+            value: 'Person',
+            operator: '='
           }
         ]
       };
