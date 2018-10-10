56cb17c95152e5810bfa11061da2d27ed7590afe
Fabrizio Schmidt <fabrizioschmidt@gmail.com>
2018-10-10 11:48:23 +0200

    * rework to array because vuex reactivity is not capable of anything

--- client/src/components/GraphList.vue
+++ client/src/components/GraphList.vue
@@ -8,7 +8,7 @@
     >
     </md-empty-state>
     <div>
-      <graph v-for="graph in Object.values(graphs)" :graph="graph"></graph>
+      <graph v-for="graph in graphs" :graph="graph"></graph>
     </div>
   </div>
 </template>
