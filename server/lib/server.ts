c433d245fc083ca7bfc468b536c466e061b6c1cd
Michael Kleen <mkleen@gmail.com>
2018-11-15 11:38:57 +0100

    Remove mongo test db

--- server/lib/server.ts
+++ server/lib/server.ts
@@ -1,10 +1,6 @@
 import app from "./app";
-import * as mongoose from "mongoose";
-import {MongoTestDataBase} from "./core/db/mongo";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-
-    new MongoTestDataBase().connect();
     console.log('Express server listening on port ' + PORT);
 });
