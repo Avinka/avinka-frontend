3979cdb839c91eefea115120922dcf9e01071492
Michael Kleen <mkleen@gmail.com>
2018-11-20 14:32:30 +0100

    Fix eslint config

--- src/api/datapointApi.js
+++ src/api/datapointApi.js
@@ -8,7 +8,6 @@ export default {
       // FIXME move&test
       const regex = /(\d+)(\S+)/gi;
       const parsedWindow = regex.exec(window);
-      console.log(parsedWindow);
       url += '&window=' + window + '&since=' + encodeURIComponent(moment().subtract(parsedWindow[1], parsedWindow[2]).toISOString()) + '&until=' + encodeURIComponent(new Date().toISOString()) + '&agg_interval=' + aggInterval;
     } else if (since && until) {
       url += '&since=' + encodeURIComponent(since.toISOString()) + '&until=' + encodeURIComponent(until.toISOString()) + '&agg_interval=' + aggInterval;
